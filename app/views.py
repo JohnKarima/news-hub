5eff6632f382e914196cf8b22e530dd85d3efade
John Karima <karimajohn24@gmail.com>
2020-09-12 14:21:45 +0300

    add categories for US and UK news in addition to Africa news

--- app/views.py
+++ app/views.py
@@ -8,9 +8,13 @@ def index():
     View root page function that returns the index page and its data
     '''
     africa_news = get_news('africa')
+    us_news = get_news('us')
+    uk_news = get_news('uk')
+
+
     print(africa_news)
     title = 'Home - Welcome to NewsHub; Global news at your fingertips'
-    return render_template('index.html', title = title, africa = africa_news)
+    return render_template('index.html', title = title, africa = africa_news, us = us_news, uk = uk_news)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
