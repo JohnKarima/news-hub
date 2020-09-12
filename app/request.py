fd17de454a267db6fad27afbe921286997740dd1
John Karima <karimajohn24@gmail.com>
2020-09-12 23:56:22 +0300

    redo API with 'sources' as an endpoint instead

--- app/request.py
+++ app/request.py
@@ -1,57 +1,53 @@
 from app import app
 import urllib.request, json
-from. models import news
+from .models import source
 
-News = news.News
+Source = source.Source
 
 
 api_key = app.config['NEWS_API_KEY']
 
 base_url = app.config['NEWS_API_BASE_URL']
 
-def get_news(category):
+def get_source(language):
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_source_url = base_url.format(language,api_key)
 
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data = url.read()
-        get_news_response = json.loads(get_news_data)
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
 
-        news_articles = None
+        source_sources = None
 
-        if get_news_response['articles']:
-            news_articles_list = get_news_response['articles']
-            news_articles = process_articles(news_articles_list)
+        if get_source_response['sources']:
+            source_sources_list = get_source_response['sources']
+            source_sources = process_sources(source_sources_list)
 
 
-    return news_articles
+    return source_sources
 
-def process_articles(news_list):
+def process_sources(source_list):
     '''
-    Function  that processes the news result and transforms them to a list of Objects
+    Function  that processes the news sources and transforms them to a list of Objects
 
     Args:
-        news_list: A list of dictionaries that contain news details
+        source_list: A list of dictionaries that contain news sources
 
     Returns :
-        news_arcicles: A list of news objects
+        source_sources: A list of news sources
     '''
-    news_articles = []
-    for news_item in news_list:
-        source = news_item.get('source')
-        author = news_item.get('author')
-        title = news_item.get('title')
-        description = news_item.get('description')
-        url = news_item.get('url')
-        urlToImage = news_item.get('urlToImage')
-        publishedAt = news_item.get('publishedAt')
-        content = news_item.get('content')
+    source_sources = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
 
+        source_object = Source(id,name,description,url,category)
+        source_sources.append(source_object)
 
-        if urlToImage:
-            news_object = News(source, author, title, description, url, urlToImage, publishedAt, content)
-            news_articles.append(news_object)
-
-    return news_articles
+      
+    return source_sources
--- app/views.py
+++ app/views.py
@@ -1,25 +1,13 @@
 from flask import render_template
 from app import app
-from .request import get_news
+from .request import get_source
 
 @app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    africa_news = get_news('africa')
-    us_news = get_news('us')
-    uk_news = get_news('uk')
-
-
-    print(africa_news)
+    sources_english = get_source('en')
+    print(sources_english)
     title = 'Home - Welcome to NewsHub; Global news at your fingertips'
-    return render_template('index.html', title = title, africa = africa_news, us = us_news, uk = uk_news)
-
-@app.route('/news/<int:news_id>')
-def news(news_id):
-
-    '''
-    View news page function that returns the news details page and its data
-    '''
-    return render_template('news.html',id = news_id)
+    return render_template('index.html', title = title, en = sources_english)
