9670a376f2b64943140602c501ead09baddc49f0
John Karima <karimajohn24@gmail.com>
2020-09-14 23:49:50 +0300

    add cards via mdncdn

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ def index():
     print(sources_english)
     print(sources_strange)
 
-    title = 'Home - Welcome to NewsHub; Global news at your fingertips'
+    title = 'Home - Welcome to NewsHub'
     return render_template('index.html', title = title, en = sources_english, fr = sources_strange)
 
 
@@ -25,9 +25,10 @@ def article(id):
     View articles page function that returns the articles details page and its data
     '''
 
+    title = 'Sources'
     article = get_article(id)
     print(article)
-    return render_template('articles.html', article = article)
+    return render_template('articles.html', title=title, article = article)
 
     
 
