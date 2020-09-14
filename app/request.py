5e8d649ad1a74906f01398d909c1f7d0d791a20a
John Karima <karimajohn24@gmail.com>
2020-09-14 07:32:15 +0300

    link to articles page

--- app/request.py
+++ app/request.py
@@ -1,8 +1,9 @@
 from app import app
 import urllib.request, json
-from .models import source
+from .models import source, article
 
 Source = source.Source
+Article = article.Article
 
 
 api_key = app.config['NEWS_API_KEY']
@@ -54,6 +55,166 @@ def process_sources(source_list):
 
       
     return source_sources
+
+
+def get_article(id):
+    '''
+    Function that gets the json response to our url request
+    '''
+  
+
+    articles_sources_url = base_url2.format(id,api_key)
+
+
+    with urllib.request.urlopen(articles_sources_url) as url:
+        get_article_data = url.read()
+        get_article_response = json.loads(get_article_data)
+
+        article_articles = None
+
+  
+
+        if get_article_response['articles']:
+            article_articles_list = get_article_response['articles']
+            article_articles = process_articles(article_articles_list)
+
+    return article_articles
+
+
+def process_articles(article_list):
+    '''
+    Function  that processes the article results and transform them to a list of Objects
+
+    Args:
+        article_list: A list of dictionaries that contain article details
+
+    Returns :
+        article_articles: A list of article objects
+    '''
+    article_articles = []
+    for article_item in article_list:
+        id = article_item.get('id')
+        name = article_item.get('name')
+        author = article_item.get('author')
+        title = article_item.get('title')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishedAt')
+        content = article_item.get('content')
+
+
+        if urlToImage:
+            article_object = Article(id,name,author,title,description, url,urlToImage,publishedAt,content)
+            article_articles.append(article_object)
+
+    return article_articles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# articles_sources_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+
+
+
+        # if get_article_response:
+        #     id = get_article_response.get('id')
+        #     name = get_article_response.get('name')
+        #     author = get_article_response.get('author')
+        #     title = get_article_response.get('title')
+        #     description = get_article_response.get('description')
+        #     url = get_article_response.get('url')
+        #     urlToImage = get_article_response.get('urlToImage')
+        #     publishedAt = get_article_response.get('publishedAt')
+        #     content = get_article_response.get('content')
+
+        #     article_object = Article(id, name, author, title, description, url, urlToImage, publishedAt, content)
+
+    # return article_articles
+
+
+
+
+        # article_articles = None
+
+
+
+
+            # id = get_article_response.get('id')
+            # name = get_article_response.get('name')
+            # author = get_article_response.get('author')
+            # title = get_article_response.get('title')
+            # description = get_article_response.get('description')
+            # url = get_article_response.get('url')
+            # urlToImage = get_article_response.get('urlToImage')
+            # publishedAt = get_article_response.get('publishedAt')
+            # content = get_article_response.get('content')
+
+            # article_object = Article(id, name, author, title, description, url, urlToImage, publishedAt, content)
+
+        # return article_articles
+
+
+
+
+
+#         article_articles_list = get_article_response['articles']
+#         article_articles = process_articles(article_articles_list)
+
+
+#     return article_articles
+
+# def process_articles(article_list):
+#     '''
+#     Function  that processes the article results and transform them to a list of Objects
+
+#     Args:
+#         article_list: A list of dictionaries that contain article details
+
+#     Returns :
+#         article_articles: A list of article objects
+#     '''
+#     article_articles = []
+#     for article_item in article_list:
+#         id = article_item.get('id')
+#         name = article_item.get('name')
+#         author = article_item.get('author')
+#         title = article_item.get('title')
+#         description = article_item.get('description')
+#         url = article_item.get('url')
+#         urlToImage = article_item.get('urlToImage')
+#         publishedAt = article_item.get('publishedAt')
+#         content = article_item.get('content')
+
+
+#         if urlToImage:
+#             article_object = Article(id,name,author,title,description, url,urlToImage,publishedAt,content)
+#             article_articles.append(article_object)
+
+#     return article_articles
+
+
 
 # def get_selected_source(id):
 #     get_source_details_url = base_url2.format(id,api_key)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_source, get_selected_source
+from .request import get_source, get_article
 
 @app.route('/')
 def index():
@@ -8,17 +8,50 @@ def index():
     View root page function that returns the index page and its data
     '''
     sources_english = get_source('en')
+    sources_strange = get_source('fr')
+
     print(sources_english)
+    print(sources_strange)
+
     title = 'Home - Welcome to NewsHub; Global news at your fingertips'
-    return render_template('index.html', title = title, en = sources_english)
+    return render_template('index.html', title = title, en = sources_english, fr = sources_strange)
 
-@app.route('/source/<int:id>')
-def source(id):
+
+
+@app.route('/articles/<id>')
+def article(id):
 
     '''
-    View source page function that returns the source details page and its data
+    View articles page function that returns the articles details page and its data
     '''
-    source = get_source(id)
-    title = f'{source.title}'
 
-    return render_template('articles.html',title = title,source = source)
+    article = get_article(id)
+    print(article)
+    return render_template('articles.html',article = article)
+
+    #title = f'{article.title}'
+    #print(article_cool)
+    #articles_cool = get_article('id')
+    #print(articles_cool)
+
+
+    
+    # title = 'yoooh brooooo - Welcome to NewsHub; Global news at your fingertips'
+
+
+
+
+
+    # articles_cool = get_article('bloomberg')
+    # print(articles_cool)
+
+    
+    # title = f'{articles.name}'
+    # title = 'not home - Welcome to NewsHub; Global news at your fingertips'
+
+    # return render_template('index.html',title = title, cnn = articles_cool)
+
+    # bloomberg_articles = get_article('bloomberg')
+    # print(bloomberg_articles)
+
+    # title = 'Articles'
